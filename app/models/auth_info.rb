583286f1e05e0f99fb43f988cf0970f8c421aae4
yoneyama ryo <uryonym@gmail.com>
2022-10-08 22:52:43 +0900

    タスクと機密情報モデルにバリデーションを設定

--- app/models/auth_info.rb
+++ app/models/auth_info.rb
@@ -1,2 +1,5 @@
 class AuthInfo < ApplicationRecord
+  # バリデーション
+  validates :service_name, presence: true
+  validates :login_id, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
+  # バリデーション
+  validates :title, presence: true
+  validates :uid, presence: true
+
+  # アソシエーション
   belongs_to :user, primary_key: :uid, foreign_key: :uid
 end
