10dc45c6498388d7dfafaeaf81b8c516c7ed93be
yoneyama ryo <uryonym@gmail.com>
2023-05-28 00:12:11 +0900

    燃費記録のルーティングを実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :tasks, except: [:show]
       resources :auth_infos, except: [:show]
+      resources :cars, except: [:show]
+      resources :refuelings, except: [:show]
     end
   end
 end
