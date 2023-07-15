1c9b82aaed4de2467dfa93088449b911b5865600
yoneyama ryo <uryonym@outlook.com>
2023-07-15 11:50:16 +0900

    ユーザー用テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_142409) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_052730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_142409) do
     t.string "description"
     t.date "dead_line"
     t.boolean "is_complete", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", primary_key: "uid", id: :string, force: :cascade do |t|
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
