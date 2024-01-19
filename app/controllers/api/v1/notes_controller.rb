a3c668e0e141614eb96ede488fbf2e20937d3a85
yoneyama ryo <uryonym@outlook.com>
2024-01-20 06:27:19 +0900

    note作成時のuidを自動指定

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -8,6 +8,7 @@ class Api::V1::NotesController < ApplicationController
 
   def create
     note = Note.new(note_params)
+    note.uid = 'MhpgUUWTcAMTpI1zvTcKRJlxysk1'
     if note.save
       render json: note
     else
