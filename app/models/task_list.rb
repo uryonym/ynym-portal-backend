4d169325f282d495bff22d43f7e0069139e74835
yoneyama ryo <uryonym@gmail.com>
2024-04-19 01:17:35 +0900

    タスクリストモデルを修正

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,8 +1,8 @@
 class TaskList < ApplicationRecord
   # バリデーション
   validates :name, presence: true
-  validates :uid, presence: true
   validates :seq, presence: true
+  validates :uid, presence: true
 
   # アソシエーション
   belongs_to :user, primary_key: :uid, foreign_key: :uid
