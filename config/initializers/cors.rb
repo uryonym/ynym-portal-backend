bc736af16b1e6a54ed04ca86d2d8bde9070f9481
yoneyama ryo <uryonym@gmail.com>
2024-08-11 23:46:02 +0900

    corx設定を更新

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5000", "localhost:5173", "portal.uryonym.com", "ynym-note.vercel.app"
+    origins "localhost:5173", "portal.uryonym.com", "ynym-portal-frontend.vercel.app"
 
     resource "*",
       headers: :any,
