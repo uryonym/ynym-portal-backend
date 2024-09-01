ec32b21f2a4796e6b1adb5ded2ef043508b8fa03
yoneyama ryo <uryonym@gmail.com>
2024-09-01 14:51:47 +0900

    ノート機能の実装に伴い、ルーティング設定を見直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
@@ -6,9 +8,11 @@ Rails.application.routes.draw do
       resources :cars do
         resources :refuelings
       end
-      resources :notes
-      resources :sections
-      resources :pages
+      resources :notes do
+        resources :sections do
+          resources :pages
+        end
+      end
     end
   end
 end
